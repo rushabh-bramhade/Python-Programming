36122dccfe0efb201e8189dff5afdab2b58a795f
Rushabh bramhade <rushabh.bramhade123@gmail.com>
2026-01-12 13:26:55 +0530

    function file push in the github

---
+++ 13_fuction.py
@@ -0,0 +1,21 @@
+# num = 10
+# def multi():
+#     result = num * 2
+#     print(result)
+# multi()
+
+
+
+ending="Thank you for coming"
+def days(name):
+    print(f"Hello {name} have a fun in these days?")
+    repaly = input(" ")
+    if(repaly =="yes"):
+        print("have a good day")
+    elif(repaly == "no"):
+        print("i wish your days well gone")
+    else:
+        print(ending)
+    
+
+days("rushabh")
